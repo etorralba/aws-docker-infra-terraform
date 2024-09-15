88f9756e32cb33934f7137677f8af59cfe760d17
etorralba <ernestotorralba@quadricular.ai>
2024-09-14 23:47:04 -0600

    💎 style: format network main tf file

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet_a" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone = "${var.region}a"
+  availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
   tags = {
     Name         = "${var.main_organization}-public-subnet-a"
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_subnet_a" {
 resource "aws_subnet" "public_subnet_b" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone = "${var.region}b"
+  availability_zone       = "${var.region}b"
   map_public_ip_on_launch = true
   tags = {
     Name         = "${var.main_organization}-public-subnet-b"
