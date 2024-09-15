093568932681a22469dfbccd8147cb4e3ac37f37
etorralba <ernestotorralba@quadricular.ai>
2024-09-14 23:46:06 -0600

    ✨ feat: add nat implementation for allowing internet access to private instances

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -7,12 +7,25 @@ resource "aws_vpc" "main_vpc" {
 }
 
 # Create Public Subnet
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet_a" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone = "${var.region}a"
   map_public_ip_on_launch = true
   tags = {
-    Name         = "${var.main_organization}-public-subnet"
+    Name         = "${var.main_organization}-public-subnet-a"
+    Organization = var.main_organization
+  }
+}
+
+# Create Public Subnet
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone = "${var.region}b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name         = "${var.main_organization}-public-subnet-b"
     Organization = var.main_organization
   }
 }
@@ -20,7 +33,7 @@ resource "aws_subnet" "public_subnet" {
 # Create Private Subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.3.0/24"
   tags = {
     Name         = "${var.main_organization}-private-subnet"
     Organization = var.main_organization
@@ -52,7 +65,46 @@ resource "aws_route_table" "public_rt" {
 }
 
 # Associate Public Subnet with Route Table
-resource "aws_route_table_association" "public_association" {
-  subnet_id      = aws_subnet.public_subnet.id
+resource "aws_route_table_association" "public_association_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_association_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_eip" "nat_eip" {
+  associate_with_private_ip = true
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_a.id
+  tags = {
+    Name         = "${var.main_organization}-nat-gateway"
+    Organization = var.main_organization
+
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name         = "${var.main_organization}-private-rt"
+    Organization = var.main_organization
+
+  }
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
 }
--- terraform/network/outputs.tf
+++ terraform/network/outputs.tf
@@ -2,8 +2,12 @@ output "vpc_id" {
   value = aws_vpc.main_vpc.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
+output "public_subnet_a_id" {
+  value = aws_subnet.public_subnet_a.id
+}
+
+output "public_subnet_b_id" {
+  value = aws_subnet.public_subnet_b.id
 }
 
 output "private_subnet_id" {
