d147a696e29737f2023148747b1aaf4024b8248c
etorralba <ernestotorralba@quadricular.ai>
2024-09-16 03:26:33 -0600

    ♻️ refactor: allow credentials on providers for pipeline

--- terraform/database/provider.tf
+++ terraform/database/provider.tf
@@ -10,10 +10,10 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-
-  profile = var.aws_access_key_id == null && var.aws_secret_access_key == null ? var.aws_profile: null
-
   region = var.region
+
+  access_key = var.aws_access_key_id != null ? var.aws_access_key_id : null
+  secret_key = var.aws_secret_access_key != null ? var.aws_secret_access_key : null
+
+  profile = var.aws_access_key_id == null && var.aws_secret_access_key == null ? var.aws_profile : null
 }
